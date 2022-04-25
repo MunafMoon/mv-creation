f6d42bf7f0fe26e71699f22e570711a4c71df344
MunafMoon <52212303+MunafMoon@users.noreply.github.com>
2022-04-25 11:40:29 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -47,7 +47,7 @@ function Navbar() {
                   <ul className={click ? 'nav-menu active' : 'nav-menu'}>
 
                       <li className='nav-item'>
-                          <Link to='/home' className='nav-links'>
+                          <Link to='/' className='nav-links'>
                               Home
                           </Link>
                       </li>
